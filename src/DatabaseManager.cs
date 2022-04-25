61fdbcd7113e69906f5c67250210fe4f91888dc4
Lucas Palumbo <lpalumbo99@gmail.com>
2022-04-25 00:55:56 -0400

    Database completed version 1.0

--- src/DatabaseManager.cs
+++ src/DatabaseManager.cs
@@ -4,6 +4,7 @@ using System.IO;
 using Windows.Storage;
 using Windows.Foundation.Collections;
 using System.Collections.ObjectModel;
+using System.Collections.Generic;
 
 namespace OpheliasOasis.src
 {
@@ -14,8 +15,9 @@ namespace OpheliasOasis.src
         
         
         // Database 
-        readonly ReservationMap   Reservations;
+        readonly ReservationMap   ReservationLookup;
         readonly PricePerDay      PriceLookup;
+        readonly CustomerIDMap    CustomerLookup;
         //readonly
         private bool _run_event_handler;
 
@@ -28,15 +30,22 @@ namespace OpheliasOasis.src
 
         public DatabaseManager()
         {
-            PriceLookup = new PricePerDay();
-            Reservations = new ReservationMap();
+            PriceLookup       = new PricePerDay();
+            ReservationLookup = new ReservationMap();
+            CustomerLookup    = new CustomerIDMap();
+
             // This needs to be false when refreshing the list from the DB
             _run_event_handler = false;
-            Reservations.MapChanged += Reservations_MapChanged;
+
+            // Add all the call back functions
+            PriceLookup.MapChanged += PriceLookup_MapChanged;
+            ReservationLookup.MapChanged += Reservations_MapChanged;
+            CustomerLookup.MapChanged += CustomerLookup_MapChanged;
 
             // File needs to be where the application is allowed to be in because
             // the application is "sandboxed" to only access that file 
             StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+            
             // No idea why I can't use a regular non asyc funtion here 
             var fileTask = storageFolder.CreateFileAsync("Database.db", CreationCollisionOption.OpenIfExists);
             fileTask.AsTask().Wait();
@@ -57,13 +66,14 @@ namespace OpheliasOasis.src
             catch (SqliteException e)
             {
                 Console.WriteLine("Program was unable to open database at: " + dbFile.Path);
-                Console.WriteLine(e.ToString());
+                System.Diagnostics.Debug.WriteLine(e.Message);
                 return;
             }
 
             // If the database is empty then it needs to be constructed
             if (new FileInfo(dbFile.Path).Length == 0)
             {
+                // Customer table
                 var cmd =
                     @"CREATE TABLE ""Customers"" (
                       ""ID""    INTEGER NOT NULL UNIQUE,
@@ -71,36 +81,69 @@ namespace OpheliasOasis.src
 	                  ""FIRST_NAME""    TEXT NOT NULL,
 	                  ""LAST_NAME"" TEXT NOT NULL,
 	                  ""PHONE"" TEXT NOT NULL,
-	                  ""CREDIT_CARD""   TEXT,
+	                  ""CREDIT_CARD_NUMBER"" TEXT,
+	                  ""CREDIT_CARD_DATE"" TEXT,
+	                  ""CREDIT_CARD_NAME"" TEXT,
 	                  PRIMARY KEY(""ID"" AUTOINCREMENT),
 	                  UNIQUE(""ID""))";
                 var command = new SqliteCommand(cmd, Connection);
                 command.ExecuteNonQuery();
 
+                // Days and Rates table
                 cmd =
                     @"CREATE TABLE ""Dates"" (
-                      ""Day""   TEXT NOT NULL UNIQUE,
-                      ""Rate""  REAL NOT NULL,
+                      ""DAY""   TEXT NOT NULL UNIQUE,
+                      ""RATE""  REAL NOT NULL,
 	                  PRIMARY KEY(""Day""))";
                 command = new SqliteCommand(cmd, Connection);
                 command.ExecuteNonQuery();
-
+                
+                // Reservation table
                 cmd =
                     @"CREATE TABLE ""Reservations"" (
                       ""ID""    INTEGER NOT NULL UNIQUE,
-                      ""TYPE""  TEXT,
-	                  ""CUSTOMER_ID""   INTEGER NOT NULL,
-	                  ""STATUS""    TEXT,
-	                  ""PAID""  INTEGER NOT NULL,
-	                  ""ROOM""  TEXT,
-	                  ""START_DATE""    TEXT NOT NULL,
-	                  ""END_DATE""  TEXT NOT NULL,
+                      ""TYPE""  TEXT NOT NULL,
+	                  ""CUSTOMER_ID"" INTEGER NOT NULL,
+	                  ""STATUS""     TEXT,
+	                  ""ROOM_ID""    TEXT,
+                      ""PRICES""     TEXT NOT NULL,
+	                  ""START_DATE"" TEXT NOT NULL,
+	                  ""END_DATE""   TEXT NOT NULL,
 	                  PRIMARY KEY(""ID"" AUTOINCREMENT))";
                 command = new SqliteCommand(cmd, Connection);
                 command.ExecuteNonQuery();
             }
             GetReservations();
+            GetPricePerDay();
+            GetCustomers();
             _run_event_handler = true;
+        }
+
+        private void PriceLookup_MapChanged(IObservableMap<DateTime, double> sender, IMapChangedEventArgs<DateTime> @event)
+        {
+            if (@event == null || !_run_event_handler) return;
+
+            switch (@event.CollectionChange)
+            {
+                case CollectionChange.Reset:
+                    _run_event_handler = false;
+                    GetPricePerDay();
+                    _run_event_handler = true;
+                    break;
+                case CollectionChange.ItemInserted:
+                    // Send back to db
+                    InsertIntoDB(sender[@event.Key], false);
+                    break;
+                case CollectionChange.ItemRemoved:
+                    throw new NotImplementedException();
+                    break;
+                case CollectionChange.ItemChanged:
+                    InsertIntoDB(sender[@event.Key], true);
+                    break;
+                // No reason? Do nothing
+                default:
+                    break;
+            }
         }
 
         // Send the change to the list back to the database
@@ -117,13 +160,13 @@ namespace OpheliasOasis.src
                     break;
                 case CollectionChange.ItemInserted:
                     // Send back to db
-
+                    InsertIntoDB(sender[@event.Key], false);
                     break;
                 case CollectionChange.ItemRemoved:
-
+                    throw new NotImplementedException();
                     break;
                 case CollectionChange.ItemChanged:
-
+                    InsertIntoDB(sender[@event.Key], true);
                     break;
                 // No reason? Do nothing
                 default:
@@ -131,14 +174,127 @@ namespace OpheliasOasis.src
             }
         }
 
-        // Can take a CustomerID, Reservation or Date Range
-        private void InsertIntoDB(object item) 
+        private void CustomerLookup_MapChanged(IObservableMap<int, Customer> sender, IMapChangedEventArgs<int> @event)
         {
-            
+            if (@event == null || !_run_event_handler) return;
+
+            switch (@event.CollectionChange)
+            {
+                case CollectionChange.Reset:
+                    _run_event_handler = false;
+                    GetCustomers();
+                    _run_event_handler = true;
+                    break;
+                case CollectionChange.ItemInserted:
+                    // Send back to db
+                    InsertIntoDB(sender[@event.Key], false);
+                    break;
+                case CollectionChange.ItemRemoved:
+                    throw new NotImplementedException();
+                    break;
+                case CollectionChange.ItemChanged:
+                    InsertIntoDB(sender[@event.Key], true);
+                    break;
+                // No reason? Do nothing
+                default:
+                    break;
+            }
+        }
+
+        // Handles changes back into the DB. Update needs to be set if the value is only being modified and not created
+        // for the first time.
+        private void InsertIntoDB(object item, bool update) 
+        {
+            if (item == null) return;
+            try
+            {
+                string cmd = "";
+                SqliteCommand command = new SqliteCommand();
+                command.Connection = Connection;
+                if (update)
+                {
+                    cmd += "UPDATE ";
+                }
+                else
+                {
+                    cmd += "INSERT INTO ";
+                }
+
+                if (item is Reservation)
+                {
+                    Reservation reservation = (Reservation)item;
+                    
+                    cmd = string.Format("Reservations(ID, TYPE, CUSTOMER_ID, STATUS, " +
+                                        "ROOM_ID, PRICES, START_DATE, END_DATE) VALUES(@id, @type, @CustomerID, @Status, @ROOM_ID, " +
+                                        "@Prices, @StartDate, @EndDate)");
+                    
+                    if (update) cmd += " WHERE ID=" + reservation.ReservationID.ToString();
+
+                    command.CommandText = cmd;
+                    command.Parameters.AddWithValue("@id", reservation.ReservationID);
+                    command.Parameters.AddWithValue("@type", reservation.Type);
+                    command.Parameters.AddWithValue("@CustomerID", reservation.CustomerID);
+                    command.Parameters.AddWithValue("@Status", reservation.Status);
+                    command.Parameters.AddWithValue("@ROOM_ID", reservation.RoomID);
+                    command.Parameters.AddWithValue("@Prices", reservation.Prices.ToString());
+                    command.Parameters.AddWithValue("@StartDate", reservation.StartDate.ToString());
+                    command.Parameters.AddWithValue("@EndDate", reservation.EndDate.ToString());
+                    
+                }
+                else if (item is Customer)
+                {
+                    Customer customer = (Customer)item;
+                    cmd = string.Format("INSERT INTO Customers(ID, EMAIL, FIRST_NAME, LAST_NAME, PHONE, CREDIT_CARD_NUMBER, CREDIT_CARD_DATE" +
+                                        "CREDIT_CARD_NAME) VALUES(@id, @email, @FirstName, @LastName, @phone, @ccnumber, @ccd, @ccname)");
+                    if (update) cmd += " WHERE ID=" + customer.Id.ToString();
+
+                    command.CommandText = cmd;
+                    command.Parameters.AddWithValue("@id",customer.Id);
+                    command.Parameters.AddWithValue("@email",customer.Email);
+                    command.Parameters.AddWithValue("@FirstName",customer.FirstName);
+                    command.Parameters.AddWithValue("@LastName",customer.LastName);
+                    command.Parameters.AddWithValue("@phone",customer.Phone);
+                    command.Parameters.AddWithValue("@ccnumber",customer.CardOnFile.CardNumbers);
+                    command.Parameters.AddWithValue("@ccd",customer.CardOnFile.ExpirationDate);
+                    command.Parameters.AddWithValue("@ccname",customer.CardOnFile.Name);
+                }
+                else if (item is Tuple<DateTime, double>)
+                {
+                    Tuple<DateTime, double> DatePrice = (Tuple<DateTime, double>)item;
+                    
+                    cmd = string.Format("Dates(DAY, RATE) VALUES(@day, @rate)");
+                    if (update) cmd += " WHERE DAY=" + DatePrice.Item1.ToString();
+
+                    command.CommandText = cmd;
+                    command.Parameters.AddWithValue("@day", DatePrice.Item1.ToString());
+                    command.Parameters.AddWithValue("@rate", DatePrice.Item2);
+                }
+                // TODO Could do some validiation check here
+                command.ExecuteNonQuery();
+            }
+            catch (SqliteException e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.Message);
+            }
+        }
+
+        private List<double> parseString(string input)
+        {
+            List<double> result = new List<double>();
+            input = input.Trim();
+            string[] splitInput = input.Split(' ');
+
+            foreach (var item in splitInput)
+            {
+                result.Add(double.Parse(item));
+            }
+
+            return result;
         }
 
         public IObservableMap<int, Reservation> GetReservations()
         {
+            _run_event_handler = false;
             var currentDate = DateTime.Today;
             var cmd         = string.Format("SELECT * FROM Reservations", currentDate.ToString(SQLTIME));
             
@@ -151,65 +307,122 @@ namespace OpheliasOasis.src
                 {
                     while (reader.Read())
                     {
+                        var res = new Reservation();
                         reservations.Add(new Reservation
                         {
-                            id         = reader.GetInt32(0),
-                            type       = reader.GetString(1),
-                            CustomerId = reader.GetInt32(2),
-                            status     = reader.GetString(3),
-                            paid       = reader.GetInt32(4),
-                            RoomId     = reader.GetInt32(5),
-                            startDate  = DateTime.Parse(reader.GetString(6)),
-                            endDate    = DateTime.Parse(reader.GetString(7)),
-
+                            ReservationID = reader.GetInt32(0),
+                            Type          = reader.GetString(1),
+                            CustomerID    = reader.GetInt32(2),
+                            Status        = reader.GetString(3),
+                            RoomID        = reader.GetInt32(4),
+                            Prices        = parseString(reader.GetString(5)),
+                            StartDate     = DateTime.Parse(reader.GetString(6)),
+                            EndDate       = DateTime.Parse(reader.GetString(7)),
                         });
                     }
                 }
             }
             catch (SqliteException e)
             {
-                System.Console.Error.WriteLine(e.ToString());
+                System.Diagnostics.Debug.WriteLine(e.Message);
             }
 
             // Only add new items. This really only should happen once during init
             foreach (var res in reservations)
             {
-                if (!Reservations.ContainsKey(res.id))
+                if (!ReservationLookup.ContainsKey(res.ReservationID))
                 { 
-                    Reservations.Add(res.id, res);
+                    ReservationLookup.Add(res.ReservationID, res);
                 }
             }
-
-            return Reservations;
+            _run_event_handler = true;
+            return ReservationLookup;
         }
 
         public IObservableMap<DateTime, double> GetPricePerDay()
         {
+            _run_event_handler = false;
             var currentDate = DateTime.Today;
-            var cmd = string.Format("SELECT * FROM Reservations", currentDate.ToString(SQLTIME));
+            var cmd = string.Format("SELECT * FROM Dates", currentDate.ToString(SQLTIME));
 
-            var priceList = new ObservableCollection<PricePerDay>();
+            var priceList = new PricePerDay();
             var command = new SqliteCommand(cmd, Connection);
 
-            // TODO I expect this to error out
             try
             {
                 using (var reader = command.ExecuteReader())
                 {
                     while (reader.Read())
                     {
-                        //TODO
-                        //DateTime date = DateTime.Parse(reader.GetString(1));
-
+                        DateTime date = DateTime.Parse(reader.GetString(1));
+                        var price = (double)reader.GetValue(2);
+                        priceList[date.Date] = price;
                     }
                 }
             }
             catch (SqliteException e)
             {
-                System.Console.Error.WriteLine(e.ToString());
+                System.Diagnostics.Debug.WriteLine(e.Message);
             }
 
+            // Only add new items. This really only should happen once during init
+            foreach (var res in priceList)
+            {
+                if (!PriceLookup.ContainsKey(res.Key))
+                {
+                    PriceLookup.Add(res.Key, res.Value);
+                }
+            }
+            _run_event_handler = true;
             return PriceLookup;
+        }
+
+        public IObservableMap<int, Customer> GetCustomers()
+        {
+            _run_event_handler = false;
+            var currentDate = DateTime.Today;
+            var cmd = string.Format("SELECT * FROM Customers", currentDate.ToString(SQLTIME));
+
+            var customerList = new List<Customer>();
+            var command = new SqliteCommand(cmd, Connection);
+
+            try
+            {
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        customerList.Add(new Customer
+                        {
+                            Id = reader.GetInt32(0),
+                            Email = reader.GetString(1),
+                            FirstName = reader.GetString(2),
+                            LastName = reader.GetString(3),
+                            Phone = reader.GetString(4),
+                            CardOnFile = new CreditCard
+                            {
+                                CardNumbers = reader.GetString(5),
+                                ExpirationDate = reader.GetString(6),
+                                Name = reader.GetString(7),
+                            }
+                        });
+                    }
+                }
+            }
+            catch (SqliteException e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.Message);
+            }
+            // Only add new items. This really only should happen once during init
+            foreach (var res in customerList)
+            {
+                if (!CustomerLookup.ContainsKey(res.Id))
+                {
+                    CustomerLookup.Add(res.Id, res);
+                }
+            }
+            _run_event_handler = true;
+            return CustomerLookup;
         }
 
     }
--- src/Types.cs
+++ src/Types.cs
@@ -7,11 +7,11 @@ using Windows.Foundation.Collections;
 
 namespace OpheliasOasis
 {
-    public class ReservationMap : IObservableMap<int, Reservation>
+    public class MapCallBack<V> : IObservableMap<int, V>
     {
-        private readonly Dictionary<int, Reservation> _items;
+        private readonly Dictionary<int, V> _items;
 
-        public event MapChangedEventHandler<int, Reservation> MapChanged;
+        public event MapChangedEventHandler<int, V> MapChanged;
 
         private class MapChangeReason : IMapChangedEventArgs<int>
         {
@@ -28,13 +28,13 @@ namespace OpheliasOasis
             MapChanged.Invoke(this, aChangeEvent);
         }
 
-        public ReservationMap()
+        public MapCallBack()
         {
-            _items = new Dictionary<int, Reservation>();
+            _items = new Dictionary<int, V>();
         }
 
         // Param: Set key to 0
-        public void Add(int key, Reservation value)
+        public void Add(int key, V value)
         {
             key = _items.Max(t => t.Key) + 1;
 
@@ -48,12 +48,12 @@ namespace OpheliasOasis
             TriggerEvent(CollectionChange.ItemInserted, key);
         }
 
-        public void Add(Reservation value)
+        public void Add(V value)
         {
             int key = _items.Max(t => t.Key) + 1;
 
             // Avoid key collision
-            while(this.ContainsKey(key))
+            while (this.ContainsKey(key))
             {
                 key++;
             }
@@ -73,23 +73,26 @@ namespace OpheliasOasis
             return (_items.Remove(key));
         }
 
-        public bool TryGetValue(int key, out Reservation value)
+        public bool TryGetValue(int key, out V value)
         {
             return _items.TryGetValue(key, out value);
         }
 
-        public Reservation this[int key] { get => _items[key];
+        public V this[int key]
+        {
+            get => _items[key];
             set
             {
                 _items[key] = value;
                 TriggerEvent(CollectionChange.ItemChanged, key);
-            } }
+            }
+        }
 
         public ICollection<int> Keys => _items.Keys.ToList();
 
-        public ICollection<Reservation> Values => _items.Values.ToList();
+        public ICollection<V> Values => _items.Values.ToList();
 
-        public void Add(KeyValuePair<int, Reservation> item)
+        public void Add(KeyValuePair<int, V> item)
         {
             TriggerEvent(CollectionChange.ItemInserted, item.Key);
             _items.Add(item.Key, item.Value);
@@ -101,15 +104,17 @@ namespace OpheliasOasis
             _items.Clear();
         }
 
-        public bool Contains(KeyValuePair<int, Reservation> item)
+        public bool Contains(KeyValuePair<int, V> item)
         {
-            if (!_items.ContainsKey((int)item.Key))
+            if (!_items.ContainsKey(item.Key))
             {
                 return false;
             }
 
-            var res = _items[item.Key];
-            if (res == null || res != item.Value)
+            var res   = _items[item.Key];
+            var _comp = item.Value;
+            // Wut?
+            if (res == null)// || res != _comp)
             {
                 return false;
             }
@@ -117,12 +122,8 @@ namespace OpheliasOasis
             return true;
         }
 
-        public void CopyTo(KeyValuePair<int, Reservation>[] array, int arrayIndex)
-        {
-            throw new NotImplementedException();
-        }
 
-        public bool Remove(KeyValuePair<int, Reservation> item)
+        public bool Remove(KeyValuePair<int, V> item)
         {
             TriggerEvent(CollectionChange.ItemRemoved, item.Key);
             return (_items.Remove(item.Key));
@@ -130,9 +131,9 @@ namespace OpheliasOasis
 
         public int Count => _items.Count();
 
-        public bool IsReadOnly => false;
+        public bool IsReadOnly => throw new NotImplementedException();
 
-        public IEnumerator<KeyValuePair<int, Reservation>> GetEnumerator()
+        public IEnumerator<KeyValuePair<int, V>> GetEnumerator()
         {
             return _items.GetEnumerator();
         }
@@ -141,19 +142,31 @@ namespace OpheliasOasis
         {
             return GetEnumerator();
         }
+
+        public void CopyTo(KeyValuePair<int, V>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ReservationMap : MapCallBack<Reservation>
+    {
+        
     }
 
     public class Reservation : INotifyPropertyChanged
     {
-        public int id { get; set; }
-        public string type { get; set; }
-        public string status { get; set; }
-        public int paid { get; set; }
-        public DateTime startDate;
-        public DateTime endDate;
-        public IDictionary<DateTime, float> pricePerDay { get; set; }
-        public int CustomerId { get; set; }
-        public int RoomId { get; set; }
+        public int ReservationID { get; set; }
+        public string Type { get; set; }
+        public string Status { get; set; }
+        
+        public DateTime StartDate;
+
+        public DateTime EndDate;
+
+        public List<double> Prices;
+        public int CustomerID { get; set; }
+        public int RoomID { get; set; }
 
         public event PropertyChangedEventHandler PropertyChanged;
         private void NotifyPropertyChanged(string propertyName)
@@ -260,6 +273,42 @@ namespace OpheliasOasis
         {
             return _pricePerDay.GetEnumerator();
         }
+    }
+
+    public class CustomerIDMap : MapCallBack<Customer>
+    {
+        
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public int ReservationID { get; set; }
+        public CreditCard CardOnFile { get; set; }
+    }
+
+    public class CreditCard
+    {
+        public string Name { get; set; }
+        public string ExpirationDate { get; set; }
+        public string CardNumbers { get; set; }
+    }
+
+    enum ReservationType
+    {
+        Conventional,
+        Prepaid,
+        
+    }
+
+    enum PaymentStatus
+    {
+        NotPaid,
+        Paid
     }
 
     enum reportType 
